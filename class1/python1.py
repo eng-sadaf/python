f7876296a8d197c2c3244f4000e9f308aad4933f
eng-sadaf <siddiqui.hadiya1@gmail.com>
2022-08-28 19:29:26 +0500

    first commit

---
+++ class1/python1.py
@@ -0,0 +1,63 @@
+"""
+print("Hello World!")
+if(5>2):
+    print("five is greater than two!")
+if(5>2):
+       print("five is greater than two")
+x=5
+y="Pakistan Zindabad!"
+print(x)
+print(y)\
+    #this is for comment
+x=5
+x="sadaf"
+print(x)
+"""
+"""
+x=int(6)
+y=str(6)
+z=float(6)
+print(x)
+print(y)
+print(z)
+print(type(x))
+print(type(y))
+print(type(z))
+x,y,z=5,"yellow","peanut"
+print(x)
+print(y)
+print(z)
+
+fruits="apple","banana","orange"
+x,y,z=fruits
+print(x)
+print(y)
+print(z)
+"""
+from operator import truediv
+
+
+x=["ornage","apple"]
+y=("orange","apple")
+print(type(x))
+print(type(y))
+x = {"name" : "John", "age" : 36}
+print(type(x))
+import random
+print(random.randrange(1,10))
+a = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+print(a)
+a = '''Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua.'''
+print(a)
+a = "Hello, World!"
+print(a[1])  #array example
+a = "Hello, World!"
+print(len(a))  #length of a string
+txt = "The best things in life are free!"
+print("free" in txt)  #To check if a certain character is present in a string
