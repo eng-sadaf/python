58dc84486d560e86a787f31c1306cb51356910b8
eng-sadaf <siddiqui.hadiya1@gmail.com>
2022-09-27 19:12:03 +0500

    commiting changes

---
+++ practice_programs/chapter8_functions/8.15/printing_models.py
@@ -0,0 +1,7 @@
+import printing_functions as pf
+
+user_info = pf.build_profile('Sadaf','Hina',
+                             location='Pakistan',
+                             field='Python',
+                             status='Married')
+print(user_info)
---
+++ practice_programs/chapter8_functions/8.16/printing_models.py
@@ -0,0 +1,11 @@
+# import printing_functions
+# from printing_functions import build_profile
+# from printing_functions import build_profile as bp
+# import printing_functions as mn
+from printing_functions import *
+
+user_info = build_profile('Sadaf','Hina',
+                             location='Pakistan',
+                             field='Python',
+                             status='Married')
+print(user_info)
